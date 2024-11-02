d7535d5a10d5259db4bfc52886f1696813e60f17
Dragon-Seeker <timekeeperguild@gmail.com>
2024-11-02 15:14:51 -0500

    Prevent issues if data pack reload fails when loading renderers

--- common/src/main/java/io/wispforest/accessories/client/AccessoriesClient.java
+++ common/src/main/java/io/wispforest/accessories/client/AccessoriesClient.java
@@ -140,6 +140,8 @@ public class AccessoriesClient {
 
     public static void init(){
         ClientLifecycleEvents.END_DATA_PACK_RELOAD.register((client, success) -> {
+            if (!success) return; // LOADING PROBLEM HAS OCCURRED SO THINGS WILL GO WRONG IF WE TRY DOING OUR STUFF
+
             AccessoriesRendererRegistry.onReload();
         });
 
